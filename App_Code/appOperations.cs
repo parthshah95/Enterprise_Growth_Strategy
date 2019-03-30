8c93e3968e6f1a09272fd3439d0bec73ce230fa7
parthshah95 <parthshahgre@gmail.com>
2019-03-30 00:41:11 -0400

    Add files via upload

---
+++ App_Code/appOperations.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SonyWeb_3
+{
+    public class appOperations
+    {
+        public static string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes
+                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+
+
+        public static string DecodeFrom64(string encodedData)
+        {
+            byte[] encodedDataAsBytes
+                = System.Convert.FromBase64String(encodedData);
+            string returnValue =
+               System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+            return returnValue;
+        }
+
+
+        // Log an Exception 
+        public static void LogException(Exception exc, string source)
+        {
+            // Include enterprise logic for logging exceptions 
+            // Get the absolute path to the log file 
+            string logFile = "Doc_Data/ErrorLog.txt";
+            logFile = HttpContext.Current.Server.MapPath(logFile);
+
+            // Open the log file for append and write the log
+            StreamWriter sw = new StreamWriter(logFile, true);
+            sw.WriteLine("********** {0} **********", DateTime.Now);
+            if (exc.InnerException != null)
+            {
+                sw.Write("Inner Exception Type: ");
+                sw.WriteLine(exc.InnerException.GetType().ToString());
+                sw.Write("Inner Exception: ");
+                sw.WriteLine(exc.InnerException.Message);
+                sw.Write("Inner Source: ");
+                sw.WriteLine(exc.InnerException.Source);
+                if (exc.InnerException.StackTrace != null)
+                {
+                    sw.WriteLine("Inner Stack Trace: ");
+                    sw.WriteLine(exc.InnerException.StackTrace);
+                }
+            }
+            sw.Write("Exception Type: ");
+            sw.WriteLine(exc.GetType().ToString());
+            sw.WriteLine("Exception: " + exc.Message);
+            sw.WriteLine("Source: " + source);
+            sw.WriteLine("Stack Trace: ");
+            if (exc.StackTrace != null)
+            {
+                sw.WriteLine(exc.StackTrace);
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+
+    }
+}
